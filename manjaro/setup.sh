c379e2b4bbc103b4916cb7a8716f6cb5a7b43917
Shun Yuan <tanshunyuan99@gmail.com>
2020-01-21 21:44:27 +0800

    Added shell script for easier distro hopping

---
+++ manjaro/setup.sh
@@ -0,0 +1,39 @@
+sudo pacman -S powerline \
+	  docker \
+	  redshift \
+	  arandr \
+	  autorandr \
+	  poetry \
+	  pyenv \
+	  xorg-xrdb \
+	  polybar \
+	  pulseaudio \
+    xorg-xinput \
+    xorg-xbacklight \
+    nodejs \
+    npm \
+
+# Install spacevim
+SPACEVIM_FILE="$HOME/.SpaceVim.d/init.toml"
+
+if [ -f "$SPACEVIM_FILE" ]; then
+    echo "$SPACEVIM_FILE exist"
+else
+    echo "$SPACEVIM_FILE does not exist"
+    curl -sLf https://spacevim.org/install.sh | bash
+    # Lsp
+    npm install -g javascript-typescript-langserver
+    pip install --user python-language-server
+
+    # Python dep
+    pip install --user flake8
+    pip install --user yapf
+fi
+
+
+#cat <<EOT >> ~/.bashrc
+#powerline-daemon -q
+#POWERLINE_BASH_CONTINUATION=1
+#POWERLINE_BASH_SELECT=1
+#. /usr/share/powerline/bindings/bash/powerline.sh
+#EOT

82128cd30b360e37a2a8788cfd2aa625932fc5e2
tanshunyuan <tanshunyuan99@gmail.com>
2020-04-18 13:30:55 +0800

    Added polybar and Xresources

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,11 +10,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
+    # MONITOR=$m polybar --reload mybar &
+    MONITOR=$m polybar --reload -c ~/.config/polybar/config.ini main 2>~/Desktop/logfile.txt &
   done
 else
-  polybar --reload mybar &
+  # polybar --reload mybar &
+  polybar --reload -c ~/.config/polybar/config.ini main 2>~/Desktop/logfile.txt &
 fi
 
-
-echo "Polybar launched..."
+# Launch bar1 and bar2
+echo "Polybar launched...";

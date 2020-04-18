a5ce7d8828a57326f0058a1dafd294822f9397bf
Shun Yuan <tanshunyuan99@gmail.com>
2020-04-18 16:30:01 +0800

    Updated spacevim dep

--- spacevim/install.sh
+++ spacevim/install.sh
@@ -1,5 +1,10 @@
 # Js
 npm install -g javascript-typescript-langserver
-
 # Ts
 npm install -g typescript-language-server
+# Css
+npm install -g vscode-css-languageserver-bin
+# Html
+npm install -g vscode-html-languageserver-bin
+
+
